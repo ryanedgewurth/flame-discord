af4cab468317929595f9906be392d45dacf2693d
Shorthorn Studios <ryan@roypenfold.co.uk>
2019-09-02 09:36:06 +0100

    lol

--- flamebot.py
+++ flamebot.py
@@ -3,18 +3,3 @@ import discord
 
 TOKEN = 'NjE3ODQ2MTkyMDYzMzgxNTQ2.XWxESw.VBm5-xbJwwWd3haj6hhmxb9aG9I'
 client = discord.Client()
-
-
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('&test2'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
-    
-    if message.content.startswith('&info'):
-        msg = 'Flame Bot is developed by ``Ryan In The Horizon#1827``. Join the Support Discord at https://discord.gg/HUZEd63!'.format(message)
-        await client.send_message(message.channel, msg)
