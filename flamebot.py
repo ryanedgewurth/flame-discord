f7faf8f95ab3f89e0ad616057722a1289234a9a0
Shorthorn Studios <ryan@roypenfold.co.uk>
2019-09-02 09:39:04 +0100

    lol

--- flamebot.py
+++ flamebot.py
@@ -3,3 +3,13 @@ import discord
 
 TOKEN = 'NjE3ODQ2MTkyMDYzMzgxNTQ2.XWxESw.VBm5-xbJwwWd3haj6hhmxb9aG9I'
 client = discord.Client()
+
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('&test'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
