b61b5565590fbfd60e2f9da8d80ac892b2d5c065
Shorthorn Studios <ryan@roypenfold.co.uk>
2019-09-03 10:22:29 +0100

    lol

--- flamebot.py
+++ flamebot.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 
 TOKEN = 'NjE3ODQ2MTkyMDYzMzgxNTQ2.XWxESw.VBm5-xbJwwWd3haj6hhmxb9aG9I'
-client = commands.Bot(command_prefix = '.')
+client = commands.Bot(command_prefix = '&')
 client.remove_command('help')
 
 
@@ -16,25 +16,25 @@ async def on_ready():
 
 
 @client.command()
-@commands.has_role('Pukka Moderator')
+@commands.has_role('Staff')
 async def clear(ctx, amount=1):
     await ctx.channel.purge(limit=amount+1)
 
 
 @client.command()
-@commands.has_role('Pukka Moderator')
+@commands.has_role('Staff')
 async def kick(ctx, member : discord.Member, *, reason=None):
     await member.kick(reason=reason)
     await ctx.channel.purge(limit=1)
 
 @client.command()
-@commands.has_role('Pukka Moderator')
+@commands.has_role('Staff')
 async def ban(ctx, member : discord.Member, *, reason=None):
     await member.ban(reason=reason)
     await ctx.channel.purge(limit=1)
 
 @client.command()
-@commands.has_role('Pukka Moderator')
+@commands.has_role('Staff')
 async def unban(ctx, *, member):
     banned_users=await ctx.guild.bans()
     member_name, member_discriminator = member.split('#')
@@ -47,7 +47,7 @@ async def unban(ctx, *, member):
         await ctx.channel.purge(limit=1)
 
 @client.command()
-@commands.has_role('Pukka Moderator')
+@commands.has_role('Staff')
 async def mute(ctx, member: discord.Member=None):
     role = discord.utils.get(ctx.guild.roles, name="Muted")
     if not member:
@@ -56,7 +56,7 @@ async def mute(ctx, member: discord.Member=None):
     await member.add_roles(role)
     await ctx.channel.purge(limit=1)
 @client.command()
-@commands.has_role('Pukka Moderator')
+@commands.has_role('Staff')
 async def unmute(ctx, member: discord.Member=None):
     role = discord.utils.get(ctx.guild.roles, name="Muted")
     if not member:
@@ -66,7 +66,7 @@ async def unmute(ctx, member: discord.Member=None):
     await ctx.channel.purge(limit=1)
 
 @client.command()
-@commands.has_role('Pukka Moderator')
+@commands.has_role('Staff')
 async def messagemanager(ctx):
     await ctx.send('You can manage messages.')
 
@@ -80,7 +80,7 @@ async def echo(ctx, *args):
     await ctx.send(output)
 
 @client.command()
-@commands.has_role('Pukka Moderator')
+@commands.has_role('Staff')
 async def count(ctx):
     count = 0
     while True:
@@ -100,7 +100,7 @@ async def help(ctx):
     embed.add_field(name='Echo', value='Echos all works after the command', inline=False)
     await ctx.send(author, embed=embed)
 @client.command()
-@commands.has_role('Pukka Moderator')
+@commands.has_role('Staff')
 async def modhelp(ctx):
     await ctx.channel.purge(limit=1)
     author = ctx.message.author
@@ -116,22 +116,7 @@ async def modhelp(ctx):
     embed.add_field(name='Unmute', value='Unmutes a member', inline=False)
     embed.add_field(name='Clear', value='Clears a specified amount of messages above', inline=False)
     await ctx.send(author, embed=embed)
-@client.command()
-async def verify(ctx):
-    author = ctx.message.author
 
-    embed = discord.Embed(
-        colour = discord.Colour.purple()
-        )
-    embed.set_author(name="Please Follow the following simple steps to be verified!")
-    embed.add_field(name='1. Join our Steam group!', value='Click [here](https://steamcommunity.com/groups/PukkaChill) to join our Steam group!', inline=False)
-    embed.add_field(name='2. Link your Steam to Discord!', value='Link your steam profile to your Discord profile so we can see that you own the account!', inline=False)
-    embed.add_field(name='3. Get to Steam level 1 and Pukka Bronze!', value='Make sure you are steam level 1 and Pukka Bronze in our Discord server, you can get Pukka Bronze after chatting for 5 minutes in #lobby', inline=False)
-    embed.add_field(name='4. Make your profile public!', value='Make sure your Steam profile is public, this is so we can view your groups and play time!', inline=False)
-    embed.add_field(name='5. DM a moderator, admin or the president!', value='DM a moderator, admin or the president after all the above is completed to get verified!', inline=False)
-
-
-    await ctx.send(author, embed=embed)
 
 
 
