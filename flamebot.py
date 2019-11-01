8576efbbf03430af55b2fb12376c45a9981ad6aa
Shorthorn Studios <ryan@roypenfold.co.uk>
2019-11-01 22:30:04 +0000

    comment out vc commands

--- flamebot.py
+++ flamebot.py
@@ -5,7 +5,6 @@ import random
 # Bot Setup
 TOKEN = 'NjE3ODQ2MTkyMDYzMzgxNTQ2.XWxESw.VBm5-xbJwwWd3haj6hhmxb9aG9I'
 client = discord.Client()
-discord.Game("&help")
 
 @client.event
 async def on_message(message):
@@ -19,7 +18,7 @@ async def on_message(message):
     # Help Command
     if message.content.startswith('&help'):
         msg = ':incoming_envelope: I have sent an message with the commands to help you.'
-        pmsg = 'List of **Flame** commands\n__***:question: Bot Information :question:***__\n``&about`` - Sends you how to contact the developer, the bot name and an support server invite.\n``&help`` - Show this Command List'
+        pmsg = 'List of **Flame** commands\n__***:question: Bot Information :question:***__\n``&ping`` - Responds with Latency (ALPHA! ONLY RESPONDS WITH PONG!)\n``&about`` - Sends you how to contact the developer, the bot name and an support server invite.\n``&help`` - Show this Command List'
         await client.send_message(message.channel, msg)
         await client.send_message(message.author, pmsg)
     # Ping Command
