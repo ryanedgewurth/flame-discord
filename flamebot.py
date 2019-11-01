b7e4f514893b413c9b0bb4c4fa1b609b2ffe7377
Shorthorn Studios <ryan@roypenfold.co.uk>
2019-11-01 13:40:49 +0000

    added test command

--- flamebot.py
+++ flamebot.py
@@ -4,8 +4,20 @@ from discord.ext import commands
 TOKEN = 'NjE3ODQ2MTkyMDYzMzgxNTQ2.XWxESw.VBm5-xbJwwWd3haj6hhmxb9aG9I'
 client = commands.Bot(command_prefix = '&')
 client.remove_command('help')
-@client.event 
-async def test(arg1):
-    await ctx.send(arg1)
+@client.event
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
 
+    if message.content.startswith('!hello'):
+        msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
 client.run(TOKEN)
