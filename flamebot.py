4ae8dfd55efefa9f6bccb757d6b8dcf528f8d3fa
Shorthorn Studios <ryan@roypenfold.co.uk>
2019-11-01 13:40:20 +0000

    added test command

--- flamebot.py
+++ flamebot.py
@@ -4,7 +4,7 @@ from discord.ext import commands
 TOKEN = 'NjE3ODQ2MTkyMDYzMzgxNTQ2.XWxESw.VBm5-xbJwwWd3haj6hhmxb9aG9I'
 client = commands.Bot(command_prefix = '&')
 client.remove_command('help')
-@client.command()  
+@client.event 
 async def test(arg1):
     await ctx.send(arg1)
 
