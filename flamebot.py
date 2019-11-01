2c64f294a332dc3134824b67c8464978ea5df213
Shorthorn Studios <ryan@roypenfold.co.uk>
2019-11-01 13:38:00 +0000

    added test command

--- flamebot.py
+++ flamebot.py
@@ -5,7 +5,7 @@ TOKEN = 'NjE3ODQ2MTkyMDYzMzgxNTQ2.XWxESw.VBm5-xbJwwWd3haj6hhmxb9aG9I'
 client = commands.Bot(command_prefix = '&')
 client.remove_command('help')
 @client.command()  
-async def test(ctx, arg1):
+async def test(arg1):
     await ctx.send(arg1)
 
 client.run(TOKEN)
