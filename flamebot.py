913620cea165ac63f687553b240db36837a1bc4a
Shorthorn Studios <ryan@roypenfold.co.uk>
2019-11-01 20:31:34 +0000

    added test command

--- flamebot.py
+++ flamebot.py
@@ -1,17 +1,21 @@
+# Imports
 import discord
+import random
 from discord.ext import commands
-
+# Bot Setup
 TOKEN = 'NjE3ODQ2MTkyMDYzMzgxNTQ2.XWxESw.VBm5-xbJwwWd3haj6hhmxb9aG9I'
-client = commands.Bot(command_prefix = '&')
-client.remove_command('help')
 @client.event
 async def on_message(message):
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
 
-    if message.content.startswith('!hello'):
+    if message.content.startswith('&hello'):
         msg = 'Hello {0.author.mention}'.format(message)
+        await client.send_message(message.channel, msg)
+    
+    if message.content.startwith('&about'):
+        msg = 'Hello. My bot name is Flame Bot and I am created by ``Edgewurth#1827``. My support discord is at https://discord.gg/Xb5asjm - join it if you need help'
         await client.send_message(message.channel, msg)
 
 @client.event
