5f48c9d91b5ef492751b13b2214702782c4df886
Shorthorn Studios <ryan@roypenfold.co.uk>
2019-11-01 13:26:50 +0000

    added test command

--- flamebot.py
+++ flamebot.py
@@ -86,6 +86,10 @@ async def count(ctx):
     while True:
         count+=1
         await ctx.send(count)
+        
+@client.command()  
+async def count(ctx):
+    await ctx.send("Test Submitted")
 
 @client.command()
 async def help(ctx):
