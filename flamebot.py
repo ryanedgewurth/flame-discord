61cc2f419c4117b38c9ebba14d72e403087a7b58
Shorthorn Studios <ryan@roypenfold.co.uk>
2019-11-02 18:08:54 +0000

    help for text channel - &help sendhere

--- flamebot.py
+++ flamebot.py
@@ -29,10 +29,17 @@ async def on_message(message):
     # Help Command
     if message.content.startswith('&help'):
         print("[FLAME] Help Command Sent")
+        args = message.content.split(" ")
+        usetxtc = arg[1]
         msg = ':incoming_envelope: I have sent an message with the commands to help you.'
         pmsg = 'List of **Flame** commands\n__***:game_die: Fun :game_die:***__\n``&8ball [question]`` - Ask the Magic 8-Ball an Question\n``&rng [minvalue] [maxvalue]`` - Random Number Generator\n__***:question: Bot Information :question:***__\n``&ping`` - Responds with Latency (ALPHA! ONLY RESPONDS WITH PONG!)\n``&about`` - Sends you how to contact the developer, the bot name and an support server invite.\n``&version`` - Get Bot Version\n``&help`` - Show this Command List'
-        await client.send_message(message.channel, msg)
-        await client.send_message(message.author, pmsg)
+        if usetxtc = sendhere:
+            await client.send_message(message.channel, pmsg)
+        else:
+            await client.send_message(message.channel, msg)
+            await client.send_message(message.author, pmsg)
+        
+        
     # Ping Command
     if message.content.startswith('&ping'):
         print("[FLAME] Ping Command Sent by ", message.author)
