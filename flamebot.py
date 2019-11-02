b83fa39015012e2bdfbf535cdbb0e08702077728
Shorthorn Studios <ryan@roypenfold.co.uk>
2019-11-02 12:17:34 +0000

    make it better

--- flamebot.py
+++ flamebot.py
@@ -24,7 +24,7 @@ async def on_message(message):
     # Help Command
     if message.content.startswith('&help'):
         msg = ':incoming_envelope: I have sent an message with the commands to help you.'
-        pmsg = 'List of **Flame** commands\n__***:question: Bot Information :question:***__\n``&ping`` - Responds with Latency (ALPHA! ONLY RESPONDS WITH PONG!)\n``&about`` - Sends you how to contact the developer, the bot name and an support server invite.\n``&help`` - Show this Command List'
+        pmsg = 'List of **Flame** commands\n__***:game_die: Fun :game_die:***__\n``&8ball [question]`` - Ask the Magic 8-Ball an Question\n__***:question: Bot Information :question:***__\n``&ping`` - Responds with Latency (ALPHA! ONLY RESPONDS WITH PONG!)\n``&about`` - Sends you how to contact the developer, the bot name and an support server invite.\n``&help`` - Show this Command List'
         await client.send_message(message.channel, msg)
         await client.send_message(message.author, pmsg)
     # Ping Command
