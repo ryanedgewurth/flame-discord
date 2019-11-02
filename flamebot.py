e351cc6e8175b999f89a84d6a7eeda428ffaf414
Shorthorn Studios <ryan@roypenfold.co.uk>
2019-11-02 14:40:11 +0000

    time section

--- flamebot.py
+++ flamebot.py
@@ -121,7 +121,7 @@ async def on_message(message):
             elif times == 30:
                 msg = '30 Seconds Left'
                 await client.send_message(message.channel, msg)
-            time.sleep(1)
+            sleep(1)
             time = times - 1
         msg = 'TIME\'S UP {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
