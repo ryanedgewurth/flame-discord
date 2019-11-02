1ff470f3f1b277dbeeb8227644cbda617cda81a5
Shorthorn Studios <ryan@roypenfold.co.uk>
2019-11-02 14:12:10 +0000

    time section

--- flamebot.py
+++ flamebot.py
@@ -63,7 +63,7 @@ async def on_message(message):
         args = message.content.split(" ")
         time = args[1]
         format = args[2]
-        msg = 'Started a Timer of ' + time format
+        msg = 'Started a Timer of ' + time + format
         await client.send_message(message.channel, msg)
         if format == 'm':
             # Secs = Mins * 60
