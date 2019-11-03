a829bb9b41a0fca57047da62f5944f8864a148e6
Shorthorn Studios <ryan@roypenfold.co.uk>
2019-11-03 21:41:37 +0000

    l

--- flamebot.py
+++ flamebot.py
@@ -118,7 +118,7 @@ async def on_message(message):
                 amount = args[1]
                 async for message in client.logs_from(message.channel, limit=int(amount) + 1):
                     msgs.append(message)
-                await client.delete_messages(msgs)
+                    await client.delete_messages(msgs)
                 await client.send_message(message.channel, 'Deleted ' + amount + ' messages')
             except IndexError:            
                 msg = ':warning: ERROR: ``Value \'amount\' needs to be filled!``'
