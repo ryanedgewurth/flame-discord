6203f07c656eb01b7e31bd4e59e1ec518ce5066b
Shorthorn Studios <ryan@roypenfold.co.uk>
2019-11-03 12:56:24 +0000

    l

--- flamebot.py
+++ flamebot.py
@@ -248,5 +248,9 @@ async def countdown_command(times, format):
     await client.send_message(message.channel, msg)
 @client.event
 async def on_ready():
+    try:
+        client.change_presence(game=discord.Game(name="&help"))
+    except:
+        print("[FLAME] Cannot Set Playing Tag")
     print("[FLAME] Bot Signed In and Started!")
 client.run(TOKEN)
