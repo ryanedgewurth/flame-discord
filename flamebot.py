f40ac13d8922edef91e6dd37522e70b725c0e566
Shorthorn Studios <ryan@roypenfold.co.uk>
2019-12-01 13:08:47 +0000

    Added Icon to Clear

--- flamebot.py
+++ flamebot.py
@@ -131,7 +131,7 @@ async def on_message(message):
             async for message in client.logs_from(message.channel, limit=int(amount) + 1):
                 msgs.append(message)
             await client.delete_messages(msgs)
-            await client.send_message(message.channel, 'Deleted ' + amount + ' messages')
+            await client.send_message(message.channel, ':wastebasket: Deleted ``' + amount + '`` messages')
         else:
             msg = ':warning: ERROR: ``You do not have the permission "Manage Messages"``'
             await client.send_message(message.channel, msg)
