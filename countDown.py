91f6aea8657e428f474c24fb534d5aed4bfe6df0
Roy Penfold <musicmaestro@users.noreply.github.com>
2020-01-06 21:46:01 +0000

    Incorporate various timing units
    Add multipliers for hours/days/weeks/months. Untested code at present - CAUTION

--- countDown.py
+++ countDown.py
@@ -1,4 +1,10 @@
 class countDown(object):
+        # Define various interval units
+        m = 60
+        h = m * 60
+        d = h * 24
+        w = d * 7
+        mo = w *4 #Assume 4 week months = 28 days/4 weeks
         ####################################################################################################
         # Initialise object with passed parameters (Number of Units, Units)
         ####################################################################################################        
@@ -12,7 +18,7 @@ class countDown(object):
         # Getter for total number of 'clicks' (normally 1/1000 of a second)
         ####################################################################################################        
         def getTotalTime(self):
-                return self.numUnits * 60 * 1000 # Hard coded to minutes for now. (See ./tests/countDownTest.py)
+                return self.numUnits * self.numTicks() * 1000 # Hard coded to minutes for now. (See ./tests/countDownTest.py)
       
         ####################################################################################################
         # Getter and setter for timer description....not yet fully implemented
@@ -35,17 +41,16 @@ class countDown(object):
         # Handle various unit types - not yet implemented
         ####################################################################################################                
         def numTicks(self):
-                print("in numTicks")
                 if self._timeFormat == 'm':
-                        return int(m)*int(self._numberOfIntervals) # I'm playing safe by typecasting here
+                        return int(m) # I'm playing safe by typecasting here
                 if self._timeFormat == 'h':
-                        return int(m)*int(self._numberOfIntervals)
+                        return int(h)
                 if self._timeFormat == 'd':
-                        return int(m)*int(self._numberOfIntervals)
+                        return int(d)
                 if self._timeFormat == 'w':
-                        return int(w)*int(self._numberOfIntervals)
+                        return int(w)
                 if self._timeFormat == 'mo':
-                        return int(mo)*int(self._numberOfIntervals)
+                        return int(mo)
                 return 0          
                         
 
