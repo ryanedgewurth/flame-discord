5536c6d4381a7b19989c1c0f2d0e05d4a3d36a04
Roy Penfold <royp@roypenfold.co.uk>
2020-01-26 15:22:38 +0000

    Trigger async countdown event

--- Flamebot.py
+++ Flamebot.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 #encoding: UTF-8
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 import random
 import asyncio
 import botToken
@@ -11,16 +11,10 @@ sys.path.append("./modules/")
 from eightBall import eightBall
 
 
-description="FlameBot"
+description = "FlameBot"
 
-bot = commands.Bot(command_prefix='?', description=description)
 
-@bot.event
-async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
+
 
 
 
@@ -28,17 +22,40 @@ class FlameClient(discord.Client):
     def __init__(self, * args, ** kwargs):
         super().__init__(*args, ** kwargs)
     
-    
-
     async def on_message(self, message):
         # we do not want the bot to reply to itself
         if message.author == client.user:
             return
         # This will be further re-factored
-        if message.content.startswith("&8ball"):
-            msg=eightBall.runCmd()
+        if message.content.startswith("^24ball"):
+            msg = eightBall.runCmd() + eightBall.runCmd() + eightBall.runCmd()
             # Note new syntax for message
             await message.channel.send(msg)
+
+   
+    
+        @tasks.loop(seconds=30.0, count=2)
+        async def slow_count():
+            # This may seem trivial for now but is critical for the countdown timer.......
+            if slow_count.current_loop == 1:
+                await message.channel.send("BOO!...........................did I get ya?")
+
+        
+
+   
+        if message.content.startswith("^boo"):
+            msg="That was scary"
+            slow_count.start()
+            await message.channel.send(msg)
+
+        
+
+        
+
+
+        bot = commands.Bot(command_prefix='?', description=description)
+        
+            
 
     # Cheap and easy functionality
     async def on_message_edit(self, before, after):
--- botToken.py
+++ botToken.py
@@ -1,1 +1,1 @@
-token=''
+token='NjQ0NjU3MzAwMzkyNTA5NDQw.Xix5gQ.7WJL6mHN3pvBSOefgy-PjdxTRzM'
