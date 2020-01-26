439d04ed1839bdaff7ee2fcbbce1655edcbab882
Roy Penfold <royp@roypenfold.co.uk>
2020-01-26 16:58:39 +0000

    All current functionality working

--- Flamebot.py
+++ Flamebot.py
@@ -15,6 +15,7 @@ description = "FlameBot - the new speedy version"
 
 bot = commands.Bot(command_prefix='^', description=description)
 
+# Log to console that bot is logged in
 @bot.event
 async def on_ready():
     print('Logged in as')
@@ -22,33 +23,34 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
+# Simple addition of two numbers showing type-checking on input
 @bot.command()
 async def add(ctx, left: int, right: int):
     """Adds two numbers together."""
     await ctx.send(left + right)
     
+# Infamous 8ball - note renamed to fit with Python's naming rules    
 @bot.command()
 async def eightball(ctx):
     await ctx.send(eightBall.runCmd())
 
-
-
-   
+# Loop that can be triggered outside of events but respond to events
+# Will expand on this later for the reminder programming
 @tasks.loop(seconds=3.0, count=2)
 async def slow_count(ctx):
 # This may seem trivial for now but is critical for the countdown timer.......
     if slow_count.current_loop == 1:
         
         await ctx.send("Reminder Alert Message")
-
+        
+# The bot command 'Reminder' to trigger reminder message for now
 @bot.command()
 async def reminder(ctx):
-    
     slow_count.start(ctx)
-  
 
-    # Cheap and easy functionality
-async def on_message_edit(self, before, after):
+# Cheap and easy functionality to flag up edited messages - could be used to 'hook' other event
+@bot.event
+async def on_message_edit(before, after):
     fmt = '**{0.author}** edited their message:\n{0.content} -> {1.content}'
     await before.channel.send(fmt.format(before, after))
 
