f081d01be26c2e721b10b745f9798d92fb572332
Roy Penfold <musicmaestro@users.noreply.github.com>
2020-01-26 14:04:56 +0000

    Add in 8ball to main

--- Flamebot.py
+++ Flamebot.py
@@ -1,20 +1,49 @@
 #!/usr/bin/env python3
 #encoding: UTF-8
 import discord
+from discord.ext import commands
+import random
 import asyncio
 import botToken
+# Permit imports from modules directory
+import sys
+sys.path.append("./modules/")
+from eightBall import eightBall
+
+
+description="FlameBot"
+
+bot = commands.Bot(command_prefix='?', description=description)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+
+
 
 class FlameClient(discord.Client):
-    async def on_ready(self):
-        print('Connected!')
-        print('Username: {0.name}\nID: {0.id}'.format(self.user))
+    def __init__(self, * args, ** kwargs):
+        super().__init__(*args, ** kwargs)
+    
+    
 
     async def on_message(self, message):
-        print(message)
+        # we do not want the bot to reply to itself
+        if message.author == client.user:
+            return
+        # This will be further re-factored
+        if message.content.startswith("&8ball"):
+            msg=eightBall.runCmd()
+            # Note new syntax for message
+            await message.channel.send(msg)
 
+    # Cheap and easy functionality
     async def on_message_edit(self, before, after):
         fmt = '**{0.author}** edited their message:\n{0.content} -> {1.content}'
         await before.channel.send(fmt.format(before, after))
-
+    
 client = FlameClient()
 client.run(botToken.token)
