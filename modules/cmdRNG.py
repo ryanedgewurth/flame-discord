2c76f868c7cd36db5f190697e4950a8e9bda06e9
Roy Penfold <royp@roypenfold.co.uk>
2020-01-28 16:20:12 +0000

    Add patch to swap random numbers if they are transposed in the parameters

--- modules/cmdRNG.py
+++ modules/cmdRNG.py
@@ -2,4 +2,9 @@ import random
 class cmdRNG:
 
     def runCmd(smallnum, largenum):
+        # If numbers are wrong way round - don't panic just swap them.
+        if (smallnum > largenum):
+            swapnum=smallnum
+            smallnum=largenum
+            largenum=swapnum
         return random.randint(int(smallnum), int(largenum))
